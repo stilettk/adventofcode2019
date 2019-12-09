d9f85c2caf0527f5d564cebe02634eac5cc01e70
Nickolay Batov <stilettk@gmail.com>
2019-12-10 01:10:41 +0300

    01

---
+++ 01/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+// 15838248
+let atLeastZero x = if x < 0 then 0 else x
+
+let rec getFuel mass = 
+    let fuel = mass / 3 - 2 |> atLeastZero
+    fuel + if fuel > 0 then getFuel fuel else 0
+
+[<EntryPoint>]
+let main argv =
+    let result = Input.modules |> List.map getFuel |> List.sum
+    Console.WriteLine result
+    0 // return an integer exit code
