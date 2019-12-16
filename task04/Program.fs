05ee54771be126a60d006fd16d1c685a62577ea0
Nickolay Batov <stilettk@gmail.com>
2019-12-17 00:37:45 +0300

    04.4

---
+++ task04/Program.fs
@@ -0,0 +1,13 @@
+﻿open System.Text.RegularExpressions
+
+let isValidPassword password =
+    Regex.IsMatch(password, "^0*1*2*3*4*5*6*7*8*9*$") && Regex.IsMatch(password, "(.)\1+")
+    
+let countValidPasswords min max =
+    seq {min..max} |> Seq.map string |> Seq.filter isValidPassword |> Seq.length
+
+[<EntryPoint>]
+let main argv =
+    let input = 134564, 585159
+    input ||> countValidPasswords |> printfn "result = %A" 
+    0 // return an integer exit code
