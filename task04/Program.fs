b8908e47d9ddfda9bd7e82df85158acb200d6c12
Nickolay Batov <stilettk@gmail.com>
2019-12-17 15:55:00 +0300

    04.2

--- task04/Program.fs
+++ task04/Program.fs
@@ -1,13 +1,29 @@
 ﻿open System.Text.RegularExpressions
 
-let isValidPassword password =
-    Regex.IsMatch(password, "^0*1*2*3*4*5*6*7*8*9*$") && Regex.IsMatch(password, "(.)\1+")
-    
-let countValidPasswords min max =
-    seq {min..max} |> Seq.map string |> Seq.filter isValidPassword |> Seq.length
+let isValidPassword1 password = Regex.IsMatch(password, "^0*1*2*3*4*5*6*7*8*9*$") && Regex.IsMatch(password, "(\d)\1+")
+
+let isValidPassword2 (password: string) =
+    let rec isValid pass minGroup currGroup =
+        match pass with
+        | x :: y :: _ when x > y -> false
+        | x :: y :: rest when x = y -> isValid (y :: rest) minGroup (currGroup + 1)
+        | _ :: rest ->
+            let minGroup =
+                if currGroup > 1 && currGroup < minGroup then currGroup
+                else minGroup
+            isValid rest minGroup 1
+        | [] -> minGroup = 2
+    isValid (password.ToCharArray() |> Array.toList) password.Length 1
+
+let countValidPasswords isValidFunc min max =
+    seq { min .. max }
+    |> Seq.map string
+    |> Seq.filter isValidFunc
+    |> Seq.length
 
 [<EntryPoint>]
 let main argv =
     let input = 134564, 585159
-    input ||> countValidPasswords |> printfn "result = %A" 
+    input ||> countValidPasswords isValidPassword1 |> printfn "result1 = %A"
+    input ||> countValidPasswords isValidPassword2 |> printfn "result2 = %A"
     0 // return an integer exit code
