e9e86664e634befb03820b2e5f62e746840226c4
Nickolay Batov <stilettk@gmail.com>
2019-12-19 19:08:37 +0300

    05.2

--- task05/Program.fs
+++ task05/Program.fs
@@ -1,12 +1,25 @@
 ﻿module Program
 
-type Operation = | Add | Multiply | Input | Output | End
+type Operation =
+    | Add
+    | Multiply
+    | Input
+    | Output
+    | JumpIfTrue
+    | JumpIfFalse
+    | LessThan
+    | Equals
+    | End
 let getOperation value =
     match value with
     | 1 -> Add
     | 2 -> Multiply
     | 3 -> Input
     | 4 -> Output
+    | 5 -> JumpIfTrue
+    | 6 -> JumpIfFalse
+    | 7 -> LessThan
+    | 8 -> Equals
     | 99 | _ -> End
 
 type ParameterMode = | Position | Immediate
@@ -36,6 +49,18 @@ let intCode input program =
         | Multiply -> (program |> setElem (program.[index + 3]) (getValue 1 * getValue 2), output) ||> exec (index + 4)
         | Input -> (program |> setElem (program.[index + 1]) input, output) ||> exec (index + 2)
         | Output -> (program, (output @ [ getValue 1 ])) ||> exec (index + 2)
+        | JumpIfTrue ->
+            let pointer = if (getValue 1) <> 0 then getValue 2 else index + 3
+            (program, output) ||> exec pointer
+        | JumpIfFalse ->
+            let pointer = if (getValue 1) = 0 then getValue 2 else index + 3
+            (program, output) ||> exec pointer
+        | LessThan ->
+            let value = if getValue 1 < getValue 2 then 1 else 0
+            (program |> setElem program.[index + 3] value, output) ||> exec (index + 4)
+        | Equals ->
+            let value = if getValue 1 = getValue 2 then 1 else 0
+            (program |> setElem program.[index + 3] value, output) ||> exec (index + 4)
         | End -> program, output
     snd (exec input 0 program [])
 
@@ -43,4 +68,6 @@ let intCode input program =
 let main argv =
     let result1 = Input.program |> Array.toList |> intCode 1
     printfn "result1=%A" result1
+    
+    Input.program |> Array.toList |> intCode 5 |> printfn "result2=%A"
     0 // return an integer exit code
