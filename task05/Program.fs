3f53b49a3a10af7066ccefe868a47020dff22dfb
Nickolay Batov <stilettk@gmail.com>
2019-12-19 03:22:15 +0300

    05.01

---
+++ task05/Program.fs
@@ -0,0 +1,50 @@
+﻿module Program
+
+type Operation = | Add | Multiply | Input | Output | End
+let getOperation value =
+    match value with
+    | 1 -> Add
+    | 2 -> Multiply
+    | 3 -> Input
+    | 4 -> Output
+    | 99 | _ -> End
+
+type ParameterMode = | Position | Immediate
+let getParameterMode value =
+    match value with
+    | 1 -> Immediate
+    | 0 | _ -> Position
+
+let setElem index newValue list =
+    list |> List.mapi (fun i value -> if i = index then newValue else value)
+
+let getInstructionLength opCode =
+    match opCode with
+    | Add | Multiply -> 4
+    | Input | Output -> 2
+    | End -> 1
+
+let intCode input program =
+    let rec exec input index (program: int list) output =
+        let exec = exec input
+        let operation = program.[index] |> sprintf "%05i"
+        let getValue instructionIndex =
+            let value = program.[index + instructionIndex]
+            let parameterMode: ParameterMode = operation.Substring(3 - instructionIndex, 1) |> (int >> getParameterMode)
+            match parameterMode with
+            | Immediate -> value
+            | Position -> program.[value]
+        let opCode: Operation = operation.Substring(3) |> int |> getOperation
+        match opCode with
+        | Add -> (program |> setElem (program.[index + 3]) (getValue 1 + getValue 2), output) ||> exec (index + 4)
+        | Multiply -> (program |> setElem (program.[index + 3]) (getValue 1 * getValue 2), output) ||> exec (index + 4)
+        | Input -> (program |> setElem (program.[index + 1]) input, output) ||> exec (index + 2)
+        | Output -> (program, (output @ [ getValue 1 ])) ||> exec (index + 2)
+        | End -> program, output
+    snd (exec input 0 program [])
+
+[<EntryPoint>]
+let main argv =
+    let result1 = Input.program |> Array.toList |> intCode 1
+    printfn "result1=%A" result1
+    0 // return an integer exit code
